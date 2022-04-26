6baf8ab11fa0404b80ed2464b9c38bddbe8446d8
Christopher Gundler <christopher@gundler.de>
2022-04-26 17:30:09 +0000

    Add implementation of Starburst

---
+++ detectors/Starburst/src/main.cpp
@@ -0,0 +1,14 @@
+#include <ommatidia/external/server.hpp>
+
+#include "Starburst.hpp"
+
+int main() {
+  ommatidia::MetaData meta(
+      "Starburst", {"Dongheng Li", "David Winfield", "Derrick Parkhurst"},
+      "https://doi.org/10.1109/CVPR.2005.531", ommatidia::License::GPL,
+      ommatidia::PredictionType::Ellipse, ommatidia::TrainingType::Unsupported,
+      ommatidia::SupportStreaming::Yes);
+  ommatidia::external::Server<Starburst> server(std::move(meta));
+  server.Run(8080);
+  return 0;
+}
