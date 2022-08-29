c5960ab9059fa44537bf38b41e32a3438003b914
Christopher Gundler <christopher@gundler.de>
2022-08-29 15:43:37 +0000

    Allow usage from any CWD

--- src/python/builder.py
+++ src/python/builder.py
@@ -2,17 +2,22 @@
 
 import re
 import argparse
-from subprocess import run, STDOUT
+from pathlib import Path
+from subprocess import run
 
 if __name__ == "__main__":
-    parser = argparse.ArgumentParser(description='Build the appropiate container for a given Dockerfile')
-    parser.add_argument('file', type=str, help='The path to the Dockerfile')
+    parser = argparse.ArgumentParser(
+        description="Build the appropiate container for a given Dockerfile"
+    )
+    parser.add_argument("file", type=str, help="The path to the Dockerfile")
     args = parser.parse_args()
 
     # Parse the arguments from the header of the file
     versions = {}
     with open(args.file, mode="r", encoding="utf8") as file:
-        regex = re.compile(r"FROM\s+ommatidia-py(?P<py>[0-9\.]+)-cv(?P<cv>[0-9\.]+)-np(?P<np>[0-9\.]+):(?P<version>[0-9\.]+)")
+        regex = re.compile(
+            r"FROM\s+ommatidia-py(?P<py>[0-9\.]+)-cv(?P<cv>[0-9\.]+)-np(?P<np>[0-9\.]+):(?P<version>[0-9\.]+)"
+        )
         required_versions = regex.match(file.read())
         version = required_versions.group("version")
         versions["VERSION_PYTHON"] = required_versions.group("py")
@@ -20,10 +25,15 @@ if __name__ == "__main__":
         versions["VERSION_NUMPY"] = required_versions.group("np")
 
     # Create the actual arguments
-    arguments = ["docker", "build", "-t", f"ommatidia-py{versions['VERSION_PYTHON']}-cv{versions['VERSION_OPENCV']}-np{versions['VERSION_NUMPY']}:{version}"]
+    arguments = [
+        "docker",
+        "build",
+        "-t",
+        f"ommatidia-py{versions['VERSION_PYTHON']}-cv{versions['VERSION_OPENCV']}-np{versions['VERSION_NUMPY']}:{version}",
+    ]
     for name, version in versions.items():
         arguments.append("--build-arg")
         arguments.append(f"{name}={version}")
     arguments.append(".")
 
-    run(arguments, check=False)
+    run(arguments, cwd=Path(__file__).parent, check=False)
